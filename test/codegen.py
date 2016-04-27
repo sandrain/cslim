fa4ba4019982eddd1a912a989043d22cc33d9d01
Hyogi Sim <sandrain@gmail.com>
2016-04-27 03:28:13 -0400

    initial files

---
+++ test/codegen.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sqlite3
+import linecache
+
+testdb = sqlite3.connect('test.db')
+
+def print_function(path, linenum, hf, cf):
+        line = linecache.getline(path, linenum)
+        hf.write('%s;\n' % line.strip())
+
+        while True:
+            line = linecache.getline(path, linenum)
+            cf.write(line)
+            if line[0] == '}':
+                break
+            linenum += 1
+
+        cf.write('\n')
+
+def gen_lib():
+    t = testdb.cursor()
+
+    hf = open('test.h', 'w')
+    cf = open('test.c', 'w')
+    cf.write('#include "test.h"\n\n')
+
+    sql = 'select func, file, line from lib order by lid asc'
+    for r in t.execute(sql):
+        print_function(r[1], r[2], hf, cf)
+
+    cf.close()
+    hf.close()
+
+def gen_compat():
+    t = testdb.cursor()
+
+    hf = open('compat.h', 'w')
+    cf = open('compat.c', 'w')
+    cf.write('#include "compat.h"\n\n')
+
+    sql = 'select func, file, line from compat order by cid asc'
+    for r in t.execute(sql):
+        print_function(r[1], r[2], hf, cf)
+
+def main():
+    gen_lib()
+    gen_compat()
+
+if __name__ == '__main__':
+    main()
---
+++ test/resolve.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import sqlite3
+
+run = 0
+cscopedb = sqlite3.connect('glusterfs.db')
+testdb = sqlite3.connect('test.db')
+compat_list = []
+
+def add_syms(sid):
+    t = testdb.cursor()
+    t.execute('insert or ignore into syms (sid) values (?)', (sid,))
+    testdb.commit()
+
+def add_compat(sid):
+    c = cscopedb.cursor()
+    c2 = cscopedb.cursor()
+    t = testdb.cursor()
+
+    rows = c.execute('select symName from symtbl where symID in ' \
+                     '(select calledID from calltbl where callerID=?)', (sid,))
+
+    for r in rows:
+        q = 'select s.symID, s.symName, f.filePath, l.linenum ' \
+            'from filestbl f natural join linestbl l natural join symtbl s ' \
+            'where symtype="$" and symName=?'
+        tmp = c2.execute(q, r)
+        for vals in tmp:
+            exist = t.execute('select id from syms where sid=?', (vals[0],))
+            if exist.fetchone() is None:
+                compat_list.append(vals)
+                add_syms(vals[0])
+
+def unresolved():
+    t = testdb.cursor()
+    row = t.execute('select count(*) from compat where resolved=0').fetchone()
+    return row[0]
+
+def resolve_compat(cid):
+    t = testdb.cursor()
+    t.execute('update compat set resolved=1 where cid=?', (cid,))
+    testdb.commit()
+
+def populate_compat():
+    global run
+
+    print '(%d) adding %d functions ..' % (run, len(compat_list))
+    run += 1
+    t = testdb.cursor()
+    t.executemany('insert or ignore into compat (sid, func, file, line) ' \
+                  'values (?,?,?,?)', compat_list)
+    testdb.commit()
+    del compat_list[:]
+
+def resolve_run():
+    t = testdb.cursor()
+
+    for row in t.execute('select cid, sid from compat where resolved=0'):
+        add_compat(row[1])
+        resolve_compat(row[0])
+
+def main():
+    global run
+
+    t = testdb.cursor()
+
+    for row in t.execute('select sid from lib'):
+        add_compat(row[0])
+
+    populate_compat()
+
+    while unresolved() > 0:
+        resolve_run()
+        populate_compat()
+
+    testdb.close()
+    cscopedb.close()
+
+if __name__ == '__main__':
+    main()
